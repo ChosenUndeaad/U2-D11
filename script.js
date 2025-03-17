8c7d76b5b6568b33051f3123b640d4d630e64c6c
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-03-17 13:52:41 +0100

    Daily exercise

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1
+
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+}
+
+//2
+
+class Pet {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
+}
